727e3709ba7af5862f95ed81c9e5883e44cfa4cb
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-19 10:10:34 -0300

    feat(divide): create tests

--- source/divide/divide.cpp
+++ source/divide/divide.cpp
@@ -1,1 +1,17 @@
 #include "currency.hpp"
+
+Currency::Currency Currency::Currency::divide(double t_value) {
+    return Currency(this->amount / t_value, precision);
+}
+
+Currency::Currency Currency::Currency::divide(int t_value) {
+    return Currency(this->amount / t_value, precision);
+}
+
+Currency::Currency Currency::Currency::divide(std::string t_value) {
+    return Currency(this->amount / std::stod(t_value), precision);
+}
+
+Currency::Currency Currency::Currency::divide(Currency t_value) {
+    return Currency(this->amount / t_value.amount, precision);
+}
--- test/divide/divide_test.cc
+++ test/divide/divide_test.cc
@@ -1,2 +1,114 @@
 #include <gtest/gtest.h>
 #include "currency.hpp"
+
+TEST(DivideTest, DivideInt) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide(100);
+
+    EXPECT_EQ(result.amount, 100);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideNegativeInt) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide(-50);
+
+    EXPECT_EQ(result.amount, -200);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideDouble) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide(100.50);
+
+    EXPECT_EQ(result.amount, 99);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideNegativeDouble) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide(-50.25);
+
+    EXPECT_EQ(result.amount, -199);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideDoubleWithMajorPrecision) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide(100.578);
+
+    EXPECT_EQ(result.amount, 99);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideDoubleWithMinorPrecision) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide(100.5);
+
+    EXPECT_EQ(result.amount, 99);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideIntString) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide("R$100");
+
+    EXPECT_EQ(result.amount, 100);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideDoubleString) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide("R$100.50");
+
+    EXPECT_EQ(result.amount, 99);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideNegativeString) {
+    Currency::Currency currency(100, 2);
+    Currency::Currency result = currency.divide("R$-50.25");
+
+    EXPECT_EQ(result.amount, -199);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideCurrency) {
+    Currency::Currency currency1(100, 2);
+    Currency::Currency currency2(100, 3);
+
+    Currency::Currency result = currency1.divide(currency2);
+
+    EXPECT_EQ(result.amount, 100);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideNegativeCurrency) {
+    Currency::Currency currency1(100, 2);
+    Currency::Currency currency2(-50, 3);
+
+    Currency::Currency result = currency1.divide(currency2);
+
+    EXPECT_EQ(result.amount, -200);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideCurrencyWithMajorPrecision) {
+    Currency::Currency currency1(100, 2);
+    Currency::Currency currency2(100, 3);
+
+    Currency::Currency result = currency1.divide(currency2);
+
+    EXPECT_EQ(result.amount, 100);
+    EXPECT_EQ(result.precision, 2);
+}
+
+TEST(DivideTest, DivideCurrencyWithMinorPrecision) {
+    Currency::Currency currency1(100, 2);
+    Currency::Currency currency2(100, 1);
+
+    Currency::Currency result = currency1.divide(currency2);
+
+    EXPECT_EQ(result.amount, 100);
+    EXPECT_EQ(result.precision, 2);
+}
