3b29442a8919997047f76a46dabf61526a47396b
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-19 09:55:41 -0300

    feat(multiply): create tests

--- source/multiply/multiply.cpp
+++ source/multiply/multiply.cpp
@@ -1,1 +1,17 @@
 #include "currency.hpp"
+
+Currency::Currency Currency::Currency::multiply(int t_value) {
+    return Currency(this->amount * t_value, this->precision);
+}
+
+Currency::Currency Currency::Currency::multiply(double t_value) {
+    return Currency(this->amount * t_value, this->precision);
+}
+
+Currency::Currency Currency::Currency::multiply(std::string t_value) {
+    return Currency(this->amount * std::stod(t_value), this->precision);
+}
+
+Currency::Currency Currency::Currency::multiply(Currency t_value) {
+    return Currency(this->amount * t_value.amount, this->precision);
+}
