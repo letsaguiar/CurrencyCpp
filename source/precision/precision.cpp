36811b9bfce43d1a9eaa482e351476aaed6f3e53
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-23 17:05:08 -0300

    test(precision): precision coverage

--- source/precision/precision.cpp
+++ source/precision/precision.cpp
@@ -1,1 +1,9 @@
 #include "currency.hpp"
+
+int Currency::Currency::getPrecision() {
+    return this->precision;
+}
+
+void Currency::Currency::setPrecision(int t_precision) {
+    this->precision = t_precision;
+}
