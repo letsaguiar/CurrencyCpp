d178554574baca69bde53179c6d873ceb799923c
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-23 17:07:18 -0300

    feat(precision): adjust methods

--- source/precision/precision.cpp
+++ source/precision/precision.cpp
@@ -5,5 +5,9 @@ int Currency::Currency::getPrecision() {
 }
 
 void Currency::Currency::setPrecision(int t_precision) {
+    // TODO: Implement toUnit and refactor
+    const double amount = this->amount / pow(10, this->precision);
+
     this->precision = t_precision;
+    this->amount = amount * pow(10, this->precision);
 }
