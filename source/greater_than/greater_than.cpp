54005644e70bf1799e39ddd1f7328078ededdf2c
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-27 08:59:07 -0300

    feat(grater_than): create tests

--- source/greater_than/greater_than.cpp
+++ source/greater_than/greater_than.cpp
@@ -1,1 +1,17 @@
 #include "currency.hpp"
+
+bool Currency::Currency::greaterThan(int t_value) {
+    return true;
+}
+
+bool Currency::Currency::greaterThan(double t_value) {
+    return true;
+}
+
+bool Currency::Currency::greaterThan(std::string t_value) {
+    return true;
+}
+
+bool Currency::Currency::greaterThan(Currency t_value) {
+    return true;
+}
