e240f093496b3f72cb5f9dcb5e6cb95d4cd3eafb
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-27 08:50:45 -0300

    feat(equals_to): create tests

--- source/equals_to/equals_to.cpp
+++ source/equals_to/equals_to.cpp
@@ -1,1 +1,17 @@
 #include "currency.hpp"
+
+bool Currency::Currency::equalsTo(int t_value) {
+    return true;
+}
+
+bool Currency::Currency::equalsTo(double t_value) {
+    return true;
+}
+
+bool Currency::Currency::equalsTo(std::string t_value) {
+    return true;
+}
+
+bool Currency::Currency::equalsTo(Currency t_value) {
+    return true;
+}
