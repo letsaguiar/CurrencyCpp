1c0d94e06d0add74b6ec6b9a180fd0dbd88d9cd6
Letícia Aguiar <leticia.aguiar@gorilainvest.com.br>
2023-02-27 08:52:26 -0300

    feat(equals_to): adjust methods

--- source/equals_to/equals_to.cpp
+++ source/equals_to/equals_to.cpp
@@ -1,17 +1,25 @@
 #include "currency.hpp"
 
 bool Currency::Currency::equalsTo(int t_value) {
-    return true;
+    const int amount = this->extractAmountHelper(t_value);
+
+    return amount == this->amount;
 }
 
 bool Currency::Currency::equalsTo(double t_value) {
-    return true;
+    const int amount = this->extractAmountHelper(t_value);
+
+    return amount == this->amount;
 }
 
 bool Currency::Currency::equalsTo(std::string t_value) {
-    return true;
+    const int amount = this->extractAmountHelper(t_value);
+
+    return amount == this->amount;
 }
 
 bool Currency::Currency::equalsTo(Currency t_value) {
-    return true;
+    t_value = this->equalizePrecisionHelper(t_value);
+
+    return t_value.amount == this->amount;
 }
